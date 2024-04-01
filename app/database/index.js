3ad2fbfb2c5709b4bce006c155e74fe1c754735b
Shoeb Sami Ahmed Ansari <92992903+soheb21@users.noreply.github.com>
2024-04-02 03:54:49 +0530

    Update index.js

--- app/database/index.js
+++ app/database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://ansarishoeb53:adil@cluster0.kxwskpf.mongodb.net/myPortfolio);
 
     } catch (error) {
         if (error) throw error;
